fcec62cc936cf744b429cb492f964f8e7a4f9062
Ahmed Ali Rashid <ahmed@beem.africa>
2023-03-06 15:56:28 +0300

    Initial commit

---
+++ src/test/java/com/irembo/api_ratel_imiter/ApiRateLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irembo.api_ratel_imiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRateLimiterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
