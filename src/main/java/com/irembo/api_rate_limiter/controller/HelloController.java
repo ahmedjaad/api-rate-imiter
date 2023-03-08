aa1a4c3e2b391d3f0685068bdd7af054c74fdc9a
Ahmed Ali Rashid <ahmedjaad40@gmail.com>
2023-03-08 21:06:44 +0300

    Simplify the protected endpoint to a simple hello work

--- src/main/java/com/irembo/api_rate_limiter/controller/HelloController.java
+++ src/main/java/com/irembo/api_rate_limiter/controller/HelloController.java
@@ -1,40 +1,29 @@
 package com.irembo.api_rate_limiter.controller;
 
-import com.irembo.api_rate_limiter.dto.RectangleDimensionsDto;
 import com.irembo.api_rate_limiter.model.TenantRateLimit;
-import com.irembo.api_rate_limiter.resource.AreaResource;
 import com.irembo.api_rate_limiter.service.TenantBucketProvider;
 import io.github.bucket4j.Bucket;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class AreaCalculationController {
+class HelloController {
     private final TenantBucketProvider tenantBucketProvider;
 
-    public AreaCalculationController(TenantBucketProvider tenantBucketProvider) {
+    public HelloController(TenantBucketProvider tenantBucketProvider) {
         this.tenantBucketProvider = tenantBucketProvider;
     }
 
-    @PostMapping(value = "/api/v1/area/rectangle")
-    public ResponseEntity<AreaResource> rectangle(
-            @RequestBody @Validated RectangleDimensionsDto dimensions,
+    @GetMapping(value = "/api/v1/hello")
+    public ResponseEntity<String> rectangle(
             @RequestHeader(value = "X-tenant-id") String tenantId
     ) {
         Bucket tenantBucket = tenantBucketProvider.getBucketByTenant(TenantRateLimit.builder().tenantId(tenantId).build());
         if (tenantBucket.tryConsume(1)) {
-            return ResponseEntity
-                    .ok(
-                            new AreaResource(
-                                    "rectangle",
-                                    dimensions.getLength().multiply(dimensions.getWidth())
-                            )
-                    );
+            return ResponseEntity.ok("Hello World");
         }
         return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
     }
