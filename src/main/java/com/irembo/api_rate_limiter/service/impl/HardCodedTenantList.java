e6c926d40f437e90250b31661d542aa0cc396487
Ahmed Ali Rashid <ahmedjaad40@gmail.com>
2023-03-09 15:14:23 +0300

    Add more Java doc HardCodedTenantList class

--- src/main/java/com/irembo/api_rate_limiter/service/impl/HardCodedTenantList.java
+++ src/main/java/com/irembo/api_rate_limiter/service/impl/HardCodedTenantList.java
@@ -15,7 +15,14 @@ import java.util.List;
  * For the sake of brevity we're hardcoding allowed limits for each tenant, but in a real-world scenario these values would come from an engine rule service or cache
  */
 public class HardCodedTenantList {
+    /**
+     * This is the rule that is applicable to the whole system, and every tenant must conform with it
+     */
     public static final Bandwidth apiServiceWideLimit = Bandwidth.classic(5, Refill.intervally(5, Duration.of(15, ChronoUnit.SECONDS)));
+    /**
+     * Hardcoded Rate limit rules (quotas) for each tenant, again in the real world scenario these would be obtained from
+     * a rule engine ( a separate service or a cache
+     */
     protected static final List<TenantRateLimit> tenantRateLimits = Arrays.asList(
             TenantRateLimit.builder()
                     .tenantId("irembo")
